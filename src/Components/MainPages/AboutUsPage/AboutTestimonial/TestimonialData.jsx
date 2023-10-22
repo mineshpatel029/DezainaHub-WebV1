4e81e64dcf7e37c38cbfede283703b3a57df1b3f
DivyankitSingh <divyankithub@gmail.com>
2023-10-23 02:16:09 +0530

    Testimonial Animations Updated

--- src/Components/MainPages/AboutUsPage/AboutTestimonial/TestimonialData.jsx
+++ src/Components/MainPages/AboutUsPage/AboutTestimonial/TestimonialData.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import MGLogo from './TestimonialImages/Market Gabru Color Palette-47.jpg'
+import HLLogo from './TestimonialImages/HermesLaw (1).png'
+import DSLogo from './TestimonialImages/Dstreat.png'
+import AALogo from './TestimonialImages/Arcadience.png'
 
-export const TestimonialData = () => {
-  return (
-    <div>TestimonialData</div>
-  )
-}
+const TestimonialData = [
+{
+  BrandName: "Hermes Law",
+  BrandFeild: "Law Firm, Delhi",
+  PersonName : "Adv Nimit Bhola",
+  Review: "Discover what our satisfied clients have to say about their experience working with DezainaHub. We take pride in delivering exceptional branding and design solutions that leave a lasting impact. Your success story could be next! Discover what our satisfied clients have to say about their experience working with DezainaHub. We take pride in delivering exceptional branding and design solutions that leave a lasting impact. Your success story could be next!",
+  logoImg: HLLogo,
+  brandColor:"#c7ad7c",
+},
+{
+  BrandName: "D'Streat",
+  BrandFeild: "Restaurent, Bangalore",
+  PersonName : "Vedant Chandel",
+  Review: "DezainaHub's branding services transformed my restaurant, D'Streat, into a standout success. Their exceptional designs not only caught the eye but also brought in more customers. Moreover, their top-notch customer service and holistic approach made the entire process smooth and delightful. It's a partnership that's truly enhanced our business, and I couldn't be happier with the results.",
+  logoImg: DSLogo,
+  brandColor:"#383431",
+},
+{
+  BrandName: "Market Gabru",
+  BrandFeild: "Finance Influencer, Bangalore",
+  PersonName : "Harshdeep Singh",
+  Review: "Discover what our satisfied clients have to say about their experience working with DezainaHub. We take pride in delivering exceptional branding and design solutions that leave a lasting impact. Your success story could be next! Discover what our satisfied clients have to say about their experience working with DezainaHub. We take pride in delivering exceptional branding and design solutions that leave a lasting impact. Your success story could be next!",
+  logoImg: MGLogo,
+  brandColor:"#36979e",
+},
+{
+  BrandName: "Arcadience Architects",
+  BrandFeild: "Architects, Chandigarh",
+  PersonName : "Ar Navneet Goyal",
+  Review: "Discover what our satisfied clients have to say about their experience working with DezainaHub. We take pride in delivering exceptional branding and design solutions that leave a lasting impact. Your success story could be next! Discover what our satisfied clients have to say about their experience working with DezainaHub. We take pride in delivering exceptional branding and design solutions that leave a lasting impact. Your success story could be next!",
+  logoImg: AALogo,
+  brandColor:"#5c4033",
+},
+
+]
 
 export default TestimonialData;
